2ca70cc71b751c849fc18d6d45b7b6b8df8b4ba3
Nigel Anderton <nigel.anderton@hitched.email>
2017-08-14 00:25:03 +0100

    Multiple connections sorted. Need to look at sockets per user

--- client/src/app/core/socket/socket.service.ts
+++ client/src/app/core/socket/socket.service.ts
@@ -8,17 +8,19 @@ export class SocketService {
     private url = 'ws://127.0.0.1:8080';
     private socket: SocketIOClient.Socket;
 
+    constructor() {
+        this.socket = io.connect(this.url, {
+            upgrade: false,
+            transports: ['websocket']
+        });
+    }
+
     sendMessage(id: string, message: object) {
         this.socket.emit(id, message);
     }
 
     getMessages(hook: string) {
         let observable = new Observable(observer => {
-
-            this.socket = io.connect(this.url, {
-                upgrade: false,
-                transports: ['websocket']
-            });
 
             this.socket.on(hook, (data: object) => {
                 observer.next(data);
